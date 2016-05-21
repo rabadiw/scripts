a6d624eb6c5522c8ad98e7da7689337a86d77b78
Wael Rabadi <Wael Rabadi>
2016-05-21 00:55:56 -0500

    Updated to 'Open Here' Menu

--- ps-open-here/ps-open-here.ps1
+++ ps-open-here/ps-open-here.ps1
@@ -3,6 +3,7 @@
 # containing sub menu entries for: 
 #     cmd, cmd(Admin), powershell, powershell(Admin)
 #
+# Warning: code provided "AS IS", use with discretion.
 
 'directory', 'directory\background', 'drive' | %{ 
     New-Item -Path "Registry::HKEY_CLASSES_ROOT\$_\shell" -Name openhere -Force |
